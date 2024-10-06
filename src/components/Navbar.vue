5691c3bec30527091214c9595e34d90978277b5f
sy_ck <zhangxy797@mail2.sysu.edu.cn>
2024-10-06 15:52:28 +0800

    Detail修改

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,7 @@
       <el-menu-item index="1" @click="goToHome">主页</el-menu-item>
       <el-menu-item index="2" @click="goToAddressOptimization">按地址优化</el-menu-item>
       <el-menu-item index="3" @click="goToFunctionOptimization">按函数优化</el-menu-item>
+      <el-menu-item index="4" @click="goToUsageGuide">使用教程</el-menu-item>
     </div>
   </el-menu>
 </template>
@@ -28,18 +29,22 @@ export default {
     goToFunctionOptimization() {
       this.$router.push({ name: 'Detail', params: { mode: 'function' } });
     },
+    goToUsageGuide() {
+      this.$router.push({ name: 'UsageGuide' });
+    },
   },
 };
 </script>
 
 <style scoped>
 .el-menu-demo {
-  background-color: #545c64;
-  color: #fff;
+  background-color: #ffffff;
+  color: #0000ff;
   padding: 10px;
   display: flex;  /* 使用Flex布局 */
   align-items: center;
   justify-content: space-between; /* 主轴两端对齐，使左侧logo与右侧菜单分别靠边 */
+  position: relative; 
 }
 
 .logo-item {
@@ -52,8 +57,14 @@ export default {
   margin-right: 20px;
 }
 
-.el-menu-demo .el-menu-item {
-  color: #fff;
+.el-menu-demo::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  height: 2px;
+  background-color: #8fbadf;
 }
 
 .menu-right {
